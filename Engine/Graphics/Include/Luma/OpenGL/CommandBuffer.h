8bfca2c69251e61b2a4abf59855816cb6bb644bd
Tiwann <tiwannbeats@gmail.com>
2026-09-08 00:26:23 +0200

    Started opengl implementation

---
+++ Engine/Graphics/Include/Luma/OpenGL/CommandBuffer.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+#include "Containers/Fifo.h"
+#include "Rendering/CommandBuffer.h"
+#include "Commands.h"
+
+namespace Luma::OpenGL
+{
+    enum class CommandBufferState
+    {
+        Invalid,
+        Closed,
+        Opened,
+        Pending,
+    };
+
+    class CommandBuffer final: public Luma::CommandBuffer
+    {
+    public:
+        bool Allocate(const CommandBufferAllocateInfo& allocateInfo) override;
+        void Free() override;
+        void SetName(StringView name) override;
+        bool Begin(const CommandBufferBeginInfo& beginInfo) override;
+        void End() override;
+        void ClearColor(const Color& color, uint32_t attachmentIndex) override;
+        void ClearDepthStencil(float depth, uint32_t stencil) override;
+        void BindGraphicsPipeline(const Luma::GraphicsPipeline& pipeline) override;
+        void BindComputePipeline(const Luma::ComputePipeline& pipeline) override;
+        void BindVertexBuffer(const Luma::Buffer& vertexBuffer, size_t offset) override;
+        void BindIndexBuffer(const Luma::Buffer& indexBuffer, size_t offset, Format indexFormat) override;
+        void BindShaderBindingSet(const Luma::Shader& shader, const Luma::ShaderBindingSet& bindingSet) override;
+        void BindMaterial(const Luma::Material& material) override;
+        void SetViewport(float x, float y, float width, float height, float minDepth, float maxDepth) override;
+        void SetScissor(int32_t x, int32_t y, int32_t width, int32_t height) override;
+        void Draw(uint32_t vertexCount, uint32_t instanceCount, uint32_t firstVertex, uint32_t firstInstance) override;
+        void DrawIndexed(uint32_t indexCount, uint32_t instanceCount, uint32_t firstIndex, int32_t vertexOffset, uint32_t firstInstance) override;
+        void DrawIndirect(const Luma::Buffer& buffer, size_t offset, uint32_t drawCount) override;
+        void DrawIndexedIndirect(const Luma::Buffer& buffer, uint64_t offset, uint32_t drawCount) override;
+        void BeginRenderPass(const Luma::RenderPassBeginInfo& renderPassBeginInfo) override;
+        void EndRenderPass() override;
+        void PushConstants(const Luma::Shader& shader, ShaderStageFlags stageFlags, size_t offset, size_t size,const void* values) override;
+        void UpdateBuffer(const Luma::Buffer& buffer, size_t offset, size_t size, const void* data) override;
+        void TextureBarrier(const Luma::TextureBarrier& barrier) override;
+        void BufferBarrier(const Luma::BufferBarrier& barrier) override;
+        void MemoryBarrier(const Luma::MemoryBarrier& barrier) override;
+        void Dispatch(uint32_t groupCountX, uint32_t groupCountY, uint32_t groupCountZ) override;
+        void DispatchIndirect(const Luma::Buffer& buffer, uint64_t offset) override;
+        void BufferCopy(const Luma::Buffer& src, const Luma::Buffer& dest, size_t srcOffset, size_t destOffset, size_t size) override;
+        void Blit(const Luma::Texture& src, const BlitRegion& srcRegion, const Luma::Texture& dest, const BlitRegion& destRegion, Filter filter) override;
+        void Blit(const Luma::Texture& src, const Luma::Texture& dest, Filter filter) override;
+        void ExecuteCommandBuffers(const Array<const Luma::CommandBuffer*>& commandBuffers) override;
+        void CopyBufferToTexture(const Luma::Buffer& src, const Luma::Texture& dest, size_t srcOffset, size_t srcSize, uint32_t arrayIndex, uint32_t mipLevel) override;
+
+    private:
+        friend class QueueImpl;
+        String m_Name;
+        CommandBufferState m_State = CommandBufferState::Closed;
+        Fifo<Command> m_Commands;
+    };
+}
