258e527e3b6d51fd83428c77298b791695168530
Tiwann <tiwannbeats@gmail.com>
2026-09-21 19:41:19 +0200

    Removed some examples

--- Example/PongGame/Source/Main.cpp
+++
@@ -1,8 +0,0 @@
-#include "GameApplication.h"
-
-int main(int argc, char** argv)
-{
-    FGameApplication game(argc, argv);
-    game.run();
-    return 0;
-}
--- Example/PongGame/Source/MenuState.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "StateMachine.h"
-
-class FMenuState : public IState
-{
-public:
-    FMenuState(FStateMachine* stateMachine): IState(stateMachine){}
-    void onUpdate(float deltaTime) override;
-};
